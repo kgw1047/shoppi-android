37b58fefedb6bc3513f48b227cf596772d21baff
jasonkim <kgw1047@gmail.com>
2022-06-22 17:03:28 +0900

    [과제 1] 요구사항 구현, Glide 라이브러리 사용법

--- app/src/main/java/com/shoppi/app/HomeFragment.kt
+++ app/src/main/java/com/shoppi/app/HomeFragment.kt
@@ -1,12 +1,17 @@
 package com.shoppi.app
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import com.bumptech.glide.Glide
+import org.json.JSONObject
 
 class HomeFragment: Fragment() {
     override fun onCreateView(
@@ -20,12 +25,30 @@ class HomeFragment: Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val button = view.findViewById<Button>(R.id.btn_enter_product_detail)
-        button.setOnClickListener {
-            findNavController().navigate(R.id.action_home_to_product_detail)
+//        val button = view.findViewById<Button>(R.id.btn_enter_product_detail)
+//        button.setOnClickListener {
+//            findNavController().navigate(R.id.action_home_to_product_detail)
 //            val transaction = parentFragmentManager.beginTransaction()
 //            transaction.add(R.id.container_main, ProductDetailFragment())
 //            transaction.commit()
-        }
+//        }
+        val toolbarTitle = view.findViewById<TextView>(R.id.toolbar_home_title)
+        val toolbarIcon = view.findViewById<ImageView>(R.id.toolbar_home_icon)
+
+        val assetLoader = AssetLoader()
+        val homeData = assetLoader.getJsonString(requireContext(), "home.json")
+        Log.d("homeData", homeData ?: "")
+
+        if(!homeData.isNullOrEmpty()) {
+            val jsonObject = JSONObject(homeData)
+            val title = jsonObject.getJSONObject("title")
+            val text = title.getString("text")
+            val iconUrl = title.getString("icon_url")
+            toolbarTitle.text = text
+            GlideApp.with(this)
+                .load(iconUrl)
+                .into(toolbarIcon)
+
+        } // jsonObject, Array 를 불러오는 과정
     }
 }
