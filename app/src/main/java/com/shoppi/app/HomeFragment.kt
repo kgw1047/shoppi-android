7f91bada3f584658d7a4a5d730af7834f7fec5e2
jasonkim <kgw1047@gmail.com>
2022-07-11 09:51:22 +0900

    [실습 1, 2] ViewPager3, JSON 데이터파싱 with gson

--- app/src/main/java/com/shoppi/app/HomeFragment.kt
+++ app/src/main/java/com/shoppi/app/HomeFragment.kt
@@ -9,8 +9,12 @@ import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.findFragment
 import androidx.navigation.fragment.findNavController
+import androidx.viewpager2.widget.ViewPager2
 import com.bumptech.glide.Glide
+import com.google.android.material.tabs.TabLayout
+import com.google.gson.Gson
 import org.json.JSONObject
 
 class HomeFragment: Fragment() {
@@ -34,20 +38,66 @@ class HomeFragment: Fragment() {
 //        }
         val toolbarTitle = view.findViewById<TextView>(R.id.toolbar_home_title)
         val toolbarIcon = view.findViewById<ImageView>(R.id.toolbar_home_icon)
+        val viewpager = view.findViewById<ViewPager2>(R.id.viewpager_home_banner)
+        val viewpagerIndicator = view.findViewById<TabLayout>(R.id.viewpager_home_banner_indicator)
 
         val assetLoader = AssetLoader()
-        val homeData = assetLoader.getJsonString(requireContext(), "home.json")
-        Log.d("homeData", homeData ?: "")
+        val homeJsonString = assetLoader.getJsonString(requireContext(), "home.json")
+        Log.d("homeData", homeJsonString ?: "")
 
-        if(!homeData.isNullOrEmpty()) {
-            val jsonObject = JSONObject(homeData)
-            val title = jsonObject.getJSONObject("title")
-            val text = title.getString("text")
-            val iconUrl = title.getString("icon_url")
-            toolbarTitle.text = text
+        if(!homeJsonString.isNullOrEmpty()) {
+            val gson = Gson()
+            val homeData = gson.fromJson(homeJsonString, HomeData::class.java)
+
+//            val jsonObject = JSONObject(homeJsonString)
+//            val title = jsonObject.getJSONObject("title")
+//            val text = title.getString("text")
+//            val iconUrl = title.getString("icon_url")
+            toolbarTitle.text = homeData.title.text
             GlideApp.with(this)
-                .load(iconUrl)
+                .load(homeData.title.iconUrl)
                 .into(toolbarIcon)
+//            val topBanners = jsonObject.getJSONArray("top_banners")
+//            val size = topBanners.length()
+//            for(index in 0 until size) {
+//                val bannerObject = topBanners.getJSONObject(index)
+//                val backgroundImageUrl = bannerObject.getString("background_image_url")
+//                val badgeObj ect = bannerObject.getJSONObject("badge")
+//                val badgeLabel = badgeObject.getString("label")
+//                val badgeBackgroundColor = badgeObject.getString("background_color")
+//                val bannerBadge = BannerBadge(badgeLabel, badgeBackgroundColor)
+//                val bannerLabel = bannerObject.getString("label")
+//                val productDetailObject = bannerObject.getJSONObject("product_detail")
+//                val productDetailBrandName = productDetailObject.getString("brand_name")
+//                val productDetailLabel = productDetailObject.getString("label")
+//                val productDetailDiscountRate = productDetailObject.getInt("discount_rate")
+//                val productDetailPrice = productDetailObject.getInt("price")
+//                val productDetailThumbnailImageUrl = productDetailObject.getString("thumbnail_image_url")
+//                val productDetailProductId = productDetailObject.getString("product_id")
+//                val bannerProductDetail = ProductDetail(
+//                    productDetailBrandName,
+//                    productDetailLabel,
+//                    productDetailDiscountRate,
+//                    productDetailPrice,
+//                    productDetailThumbnailImageUrl,
+//                    productDetailProductId
+//                )
+//
+//
+//                val banner = Banner(
+//                    backgroundImageUrl,
+//                    bannerBadge,
+//                    bannerLabel,
+//                    bannerProductDetail
+//                )
+//
+//
+//
+//            }
+
+            viewpager.adapter = HomeBannerAdapter().apply {
+                submitList(homeData.topBanners)
+            }
         } // jsonObject, Array 를 불러오는 과정
     }
 }
